96e0eeef94e0abbad791c4e3d312115df39830a8
ryoma <ryoman1002@icloud.com>
2025-11-12 10:30:07 +0900

    windowClasわけ途中

--- Project1/Project1/window.h
+++ Project1/Project1/window.h
@@ -1,5 +1,16 @@
 #pragma once
-class window
+
+#include<Windows.h>
+#include<string>
+
+
+class window final
 {
+public:
+	window() = default;
+
+	~window() = default;
+
+	[[nodiscard]] HRESULT create(HINSTANCE instance,int width,int height, std::string_view name)
 };
 
